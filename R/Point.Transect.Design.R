cae429a6be8695cc51b73338ff02936d03005fdd
Laura Marshall <L_H_Marshall@yahoo.co.uk>
2019-05-16 18:39:30 +0100

    Move iteration over strata out of sub method to allow mix of point transect designs

--- R/Point.Transect.Design.R
+++ R/Point.Transect.Design.R
@@ -63,12 +63,104 @@ setMethod(
   f="generate.transects",
   signature="Point.Transect.Design",
   definition=function(object, silent = FALSE, ...){
-    if(object@design == "systematic"){
-      transects <- generate.systematic.points(object, ...)
+    # Process additional arguments
+    additional.args <- list(...)
+    for.coverage <- ifelse("for.coverage" %in% names (additional.args), additional.args$for.coverage, FALSE)
+    # Get strata names
+    region <- object@region
+    sf.column <- attr(region@region, "sf_column")
+    if(length(region@strata.name) > 0){
+      strata.names <- region@strata.name
+      strata.no <- length(region@strata.name)
     }else{
-      message("This design is not supported at present")
-      transects = NULL
+      strata.names <- region@region.name
+      strata.no <- 1
     }
-    return(transects)
+    #Get a vector of designs
+    if(length(object@design) == 1){
+      object@design <- rep(object@design, strata.no)
+    }else{
+      object@design <- object@design
+    }
+    #Calculate effort allocation if using only one design and only if spacing has not been specified.
+    if(length(unique(object@design)) == 1  && length(object@spacing) == 0){
+      if(length(object@effort.allocation) == 0){
+        #Use area
+        effort.allocation <- region@area/sum(region@area)
+      }else{
+        effort.allocation <- object@effort.allocation
+      }
+    }
+    #Extract design parameters
+    spacing <- object@spacing
+    no.samplers <- object@no.samplers
+    #Check if only has one has been
+    if(length(spacing) == 1){
+      spacing <- rep(spacing, strata.no)
+      by.spacing <- rep(TRUE, strata.no)
+    }else if(length(spacing) == strata.no){
+      by.spacing <- ifelse(is.na(spacing), FALSE, TRUE)
+    }else if(length(spacing) == 0){
+      by.spacing = rep(FALSE, strata.no)
+    }
+    #If spacing has not been provided for any
+    if(all(!by.spacing)){
+      #If only a total number of samplers has been provided (and there is only one design)
+      if(length(no.samplers) == 1 && length(object@design) == 1){
+        #Calculate spacing from the number of desired samplers across whole region
+        spacing <- sum(abs(region@area))^0.5 / object@no.samplers^0.5
+        spacing <- rep(spacing, strata.no)
+      }
+    #If spacing has been provided for some but not all need to calculate spacing for others
+    }else if(any(!by.spacing) && !all(!by.spacing)){
+      for(strat in seq(along = strata.names)){
+        if(!by.spacing[strat]){
+          spacing[strat] <- abs(region@area[strat])^0.5 / object@no.samplers[strat]^0.5
+        }
+      }
+    }
+    #Store all lines in a list
+    transects <- list()
+    #Iterate over strata calling the appropriate method for the design.
+    #Main grid generation
+    for (strat in seq(along = region@region[[sf.column]])) {
+      if(object@design[strat] %in% c("systematic")){
+        transects[[strat]] <- generate.systematic.points(design = object, strata.id = strat, spacing = spacing[strat], for.coverage = for.coverage)
+      #}else if(object@design[strat] == "random"){
+      #  transects[[strat]] <- generate.random.points(object, strat, no.samplers[strat], line.length[strat], spacing[strat], by.spacing[strat])
+      }else{
+        message("This design is not supported at present")
+        transects[[strat]] = NULL
+      }
+    }
+    #Put transects into a miltipart multipoint sf object defined by
+    #the strata name
+    counter <- 1
+    transect.count <- 0
+    #Find first strata where there are transects
+    while(is.na(transects[[counter]]) && counter < length(transects)){
+      counter <- counter + 1
+      cat(counter)
+    }
+    #If there are some transects somewhere
+    if(!is.na(transects[[counter]][[1]])){
+      transect.count <- dim(transects[[counter]])[1]
+      temp <- sf::st_sfc(transects[[counter]])
+      #Now add in transects from other strata
+      if(length(transects) > counter){
+        for(strat in (counter+1):length(transects)){
+          if(!is.na(transects[[strat]][[1]])){
+            transect.count <- transect.count + dim(transects[[strat]])[1]
+            temp <- c(temp, sf::st_sfc(transects[[strat]]))
+          }
+        }
+      }
+      all.transects <- st_sf(data.frame(strata = strata.names, geom = temp))
+    }else{
+      all.transects <- list()
+    }
+    #Make a survey object
+    survey <- new(Class="Point.Transect.Survey", design = object@design, points = all.transects, no.samplers = transect.count, effort.allocation = object@effort.allocation, spacing = spacing, design.angle = object@design.angle, edge.protocol = object@edge.protocol)
+    return(survey)
   }
 )
--- R/generate.systematic.points.R
+++ R/generate.systematic.points.R
@@ -1,109 +1,46 @@
-generate.systematic.points <- function(design, ...){
-  # Process additional arguments
-  additional.args <- list(...)
-  for.coverage <- ifelse("for.coverage" %in% names (additional.args), additional.args$for.coverage, FALSE)
-  # Get strata names
+generate.systematic.points <- function(design, strata.id, spacing, for.coverage = FALSE){
+  #Generates either random or systematic parallel lines
   region <- design@region
-  if(length(region@strata.name) > 0){
-    strata.names <- region@strata.name
-    strata.no <- length(region@strata.name)
-  }else{
-    strata.names <- region@region.name
-    strata.no <- 1
+  #Get the current strata and spacing
+  sf.column <- attr(region@region, "sf_column")
+  strata <- region@region[[sf.column]][[strata.id]]
+  sspace <- spacing
+  #Spin round so design angle lies along x axis
+  rot.angle.rad <- design@design.angle[strata.id]/180*pi
+  theta <- ifelse(rot.angle.rad == 0, 0, 2*pi-rot.angle.rad)
+  rot.mat <- matrix(c(cos(theta), sin(theta), -sin(theta), cos(theta)), ncol = 2, byrow = FALSE)
+  rot.strata <- strata*rot.mat
+  #Buffer strata for plus sampling?
+  if(design@edge.protocol[strata.id] == "plus"){
+    rot.strata <- st_buffer(rot.strata, design@truncation)
   }
-  # Check if the user specified the no.of samplers rather than spacing
-  if(length(design@spacing) == 0){
-    if(length(strata.names) > 1){
-      if(length(design@no.samplers) == 1){
-        #Need to allocate no.samplers to strata
-        if(length(design@effort.allocation) == 0){
-          #Use area
-          effort.allocation <- region@area/sum(region@area)
-        }else{
-          effort.allocation <- design@effort.allocation
-        }
-        no.samplers <- design@no.samplers*effort.allocation
-      }else{
-        no.samplers <- design@no.samplers
-      }
-    }
-    #Calculate spacing from the number of desired samplers
-    spacing <- abs(region@area)^0.5 / design@no.samplers^0.5
+  #Find the minimum and maximum x and y values
+  bbox <- st_bbox(rot.strata)
+  #Check spacing is appropriate
+  if(sspace > (bbox[["xmax"]]-bbox[["xmin"]]) || sspace > (bbox[["ymax"]]-bbox[["ymin"]])){
+    warning(paste("The spacing allocated to strata number ", strata.id, " is larger than either or both of the x or y dimensions of the region. Cannot generate samplers in this strata.", sep = ""), call. = FALSE, immediate. = TRUE)
+    transects <- NA
   }else{
-    spacing <- design@spacing
-  }
-  #Store all point in a list
-  transects <- list()
-  #Main grid generation
-  for (strat in seq(along = region@region$geometry)) {
-    #Get the current strata and spacing
-    strata <- region@region$geometry[[strat]]
-    sspace <- spacing[strat]
-    #Spin round so design angle lies along x axis
-    rot.angle.rad <- design@design.angle[strat]/180*pi
-    theta <- ifelse(rot.angle.rad == 0, 0, 2*pi-rot.angle.rad)
-    rot.mat <- matrix(c(cos(theta), sin(theta), -sin(theta), cos(theta)), ncol = 2, byrow = FALSE)
-    rot.strata <- strata*rot.mat
-    #Buffer strata for plus sampling?
-    if(design@edge.protocol[strat] == "plus"){
-      rot.strata <- st_buffer(rot.strata, design@truncation)
-    }
-    #Find the minimum and maximum x and y values
-    bbox <- st_bbox(rot.strata)
-    #Check spacing is appropriate
-    if(sspace > (bbox[["xmax"]]-bbox[["xmin"]]) || sspace > (bbox[["ymax"]]-bbox[["ymin"]])){
-      warning(paste("The spacing allocated to strata ", strata.names[strat], " is larger than either or both of the x or y dimensions of the region. Cannot generate samplers in this strata.", sep = ""), call. = FALSE, immediate. = TRUE)
-      transects[[strat]] <- NA
+    if(for.coverage){
+      x.diff <- bbox[["xmax"]]-bbox[["xmin"]]
+      y.diff <- bbox[["ymax"]]-bbox[["ymin"]]
+      start.x <- bbox[["xmin"]] + (x.diff - floor(x.diff/sspace)*sspace)/2
+      start.y <- bbox[["ymin"]] + (y.diff - floor(y.diff/sspace)*sspace)/2
     }else{
-      if(for.coverage){
-        x.diff <- bbox[["xmax"]]-bbox[["xmin"]]
-        y.diff <- bbox[["ymax"]]-bbox[["ymin"]]
-        start.x <- bbox[["xmin"]] + (x.diff - floor(x.diff/sspace)*sspace)/2
-        start.y <- bbox[["ymin"]] + (y.diff - floor(y.diff/sspace)*sspace)/2
-      }else{
-        start.x <- bbox[["xmin"]] + runif(1, 0, sspace)
-        start.y <- bbox[["ymin"]] + runif(1, 0, sspace)
-      }
-      x.vals <- seq(start.x, bbox[["xmax"]], by = sspace)
-      y.vals <- seq(start.y, bbox[["ymax"]], by = sspace)
-      temp.coords <- expand.grid(x.vals, y.vals)
-      #keep everything within the polygon strata
-      points <- st_multipoint(as.matrix(temp.coords))
-      to.keep <- st_intersection(points, rot.strata)
-      #Rotate back again
-      reverse.theta <- rot.angle.rad
-      rot.mat.rev <- matrix(c(cos(reverse.theta), sin(reverse.theta), -sin(reverse.theta), cos(reverse.theta)), ncol = 2, byrow = FALSE)
-      points.unrotated <- to.keep*rot.mat.rev
-      transects[[strat]] <- points.unrotated
+      start.x <- bbox[["xmin"]] + runif(1, 0, sspace)
+      start.y <- bbox[["ymin"]] + runif(1, 0, sspace)
     }
+    x.vals <- seq(start.x, bbox[["xmax"]], by = sspace)
+    y.vals <- seq(start.y, bbox[["ymax"]], by = sspace)
+    temp.coords <- expand.grid(x.vals, y.vals)
+    #keep everything within the polygon strata
+    points <- st_multipoint(as.matrix(temp.coords))
+    to.keep <- st_intersection(points, rot.strata)
+    #Rotate back again
+    reverse.theta <- rot.angle.rad
+    rot.mat.rev <- matrix(c(cos(reverse.theta), sin(reverse.theta), -sin(reverse.theta), cos(reverse.theta)), ncol = 2, byrow = FALSE)
+    points.unrotated <- to.keep*rot.mat.rev
+    transects <- points.unrotated
   }
-  #Put transects into a miltipart multipoint sf object defined by
-  #the strata name
-  counter <- 1
-  transect.count <- 0
-  #Find first strata where there are transects
-  while(is.na(transects[[counter]]) && counter < length(transects)){
-    counter <- counter + 1
-    cat(counter)
-  }
-  #If there are some transects somewhere
-  if(!is.na(transects[[counter]][[1]])){
-    transect.count <- dim(transects[[counter]])[1]
-    temp <- sf::st_sfc(transects[[counter]])
-    #Now add in transects from other strata
-    if(length(transects) > counter){
-      for(strat in (counter+1):length(transects)){
-        if(!is.na(transects[[strat]][[1]])){
-          transect.count <- transect.count + dim(transects[[strat]])[1]
-          temp <- c(temp, sf::st_sfc(transects[[strat]]))
-        }
-      }
-    }
-    all.transects <- st_sf(data.frame(strata = strata.names, geom = temp))
-  }else{
-    all.transects <- list()
-  }
-  #Make a survey object
-  survey <- new(Class="Point.Transect.Survey", design = design@design, points = all.transects, no.samplers = transect.count, effort.allocation = design@effort.allocation, spacing = spacing, design.angle = design@design.angle, edge.protocol = design@edge.protocol)
-  return(survey)
+  return(transects)
 }
