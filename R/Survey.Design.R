bf82de839936fdb93a63146eda3d2830032c9e98
Laura Marshall <L_H_Marshall@yahoo.co.uk>
2019-06-10 01:15:18 +0100

    Update Survey.Design.R
    Remove vlaidity checking
    Add show method
    Modify plot method

--- R/Survey.Design.R
+++ R/Survey.Design.R
@@ -23,6 +23,12 @@
 #' @slot truncation Object of class \code{"numeric"}; The maximum distance
 #' at which observations can be made. This is used to determine the covered
 #' area during the coverage calculations.
+#' @slot coverage.grid The coverage grid used to assess the uniformity
+#' of coverage during simulations.
+#' @slot coverage.scores The average number of times each point in the
+#' coverage grid is included in a survey.
+#' @slot design.statistics A list of values obtained when investigating
+#' coverage. This includes the minimum, maximum, mean and median
 #' @keywords classes
 #' @export
 #' @importFrom methods validObject
@@ -41,53 +47,6 @@ setClass(Class = "Survey.Design",
                                          design.statistics = "list", "VIRTUAL")
 )
 
-setValidity("Survey.Design",
-            function(object){
-              #Check how many strata there are
-              strata.count <- length(object@region@strata.name)
-              #EFFORT ALLOCATION
-              if(length(object@effort.allocation) > 0){
-                if(sum(object@effort.allocation, na.rm = T) != 1){
-                  return("Effort allocation should either be omitted or sum to 1")
-                }
-                if(any(is.na(object@effort.allocation))){
-                  return("Sorry, effort allocation is only applied across all strata at present. NA values are not permitted.")
-                }
-              }
-              #TRUNCATION
-              if(length(object@truncation) > 1){
-                warning("You have supplied more than one truncation value. Currently the same truncation value must be applied across the entire study region. Using only the first value supplied.")
-                object@truncation <- object@truncation[1]
-              }else if(object@truncation <= 0){
-                return("The truncation distance must be > 0.")
-              }
-              #Check edge protocol
-              if(length(object@edge.protocol) == 1){
-                object@edge.protocol <- rep(object@edge.protocol, strata.count)
-              }else if(length(object@edge.protocol) > 1 && length(object@edge.protocol) != strata.count){
-                warning("Edge protocol argument has a different number of values than there are strata, only using the 1st value.")
-                object@edge.protocol <- rep(object@edge.protocol[1], strata.count)
-              }
-              if(!all(object@edge.protocol %in% c("minus", "plus"))){
-                warning("Some edge protocol option(s) not recognised using minus sampling for these strata.", call. = FALSE)
-                index <- which(!(object@edge.protocol %in% c("minus", "plus")))
-                object@edge.protocol[index] <- "minus"
-              }
-              #DESIGN ANGLE
-              #Check the design angle
-              if(length(object@design.angle) == 1){
-                object@design.angle <- rep(object@design.angle, strata.count)
-              }else if(length(object@design.angle) > 1 && length(object@design.angle) != strata.count){
-                warning("Design angle argument has a different number of values than there are strata, only using the 1st value.")
-                object@design.angle <- rep(object@design.angle[1], strata.count)
-              }
-              if(any(object@design.angle < 0 || object@design.angle >= 180)){
-                return("The design angle should be >= 0 and < 180 degrees.")
-              }
-              return(TRUE)
-            }
-)
-
 
 # GENERIC METHODS DEFINITIONS --------------------------------------------
 
@@ -100,6 +59,9 @@ setValidity("Survey.Design",
 #' @param ... other general plot parameters
 #' @rdname plot.Survey.Design-methods
 #' @exportMethod plot
+#' @importFrom plot3D colkey
+#' @importFrom graphics par
+#' @importFrom grDevices heat.colors
 setMethod(
   f="plot",
   signature="Survey.Design",
@@ -117,6 +79,7 @@ setMethod(
     plot(x@region@region$geometry, main = "Coverage Scores", cex.main = 1.5)
     cols <- heat.colors(col.breaks)[as.numeric(cut(coverage.scores, breaks = col.breaks))]
     plot(x@coverage.grid@grid$geometry, pch = 20, col = cols, add = T)
+    plot(x@region@region$geometry, add = T)
     plot3D::colkey(side = 4, clim = range(coverage.scores), col = heat.colors(col.breaks), add = TRUE, length = 0.7)
     invisible(x)
   }
@@ -125,10 +88,9 @@ setMethod(
 
 #' show
 #'
-#' Summarises an S4 object of class 'Survey.Design'
+#' Summarises and displays an S4 object of class 'Survey.Design'
 #'
-#' @param x object of class Survey.Design
-#' @param y not used
+#' @param object an object which inherits from the Survey.Design class
 #' @param ... other general plot parameters
 #' @rdname show.Survey.Design-methods
 #' @exportMethod show
@@ -136,7 +98,19 @@ setMethod(
   f="show",
   signature="Survey.Design",
   definition=function(object){
-    print(object@design.statistics)
+    design.stats <- object@design.statistics
+    names.stats <- names(design.stats)
+    for(i in seq(along = design.stats)){
+     title <- switch(names.stats[i],
+                     "sampler.count" = "Number of samplers:",
+                     "cov.area" = "Covered area:",
+                     "p.cov.area" = "% of region covered:",
+                     "line.length" = "Line length:")
+     cat("\n   ", title, fill = T)
+     underline <- paste(rep("", (nchar(title)-3)), collapse = "")
+     cat("   ", underline, fill = T)
+     print(design.stats[[i]])
+    }
   }
 )
 
